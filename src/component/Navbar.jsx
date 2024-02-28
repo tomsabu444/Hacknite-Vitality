2c65de33555c41e424b67cc1b1b4d276f45a18e6
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-28 12:09:15 +0530

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,25 +4,27 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <>
-      <h1>LOGO</h1>
-      <ul>
-        <li>
-          <a href="#" id="one">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#" class="login">
-            <i class="fa-solid fa-right-to-bracket"></i>
-          </a>
-        </li>
-      </ul>
+      <div>
+        <h1>LOGO</h1>
+        <ul>
+          <li>
+            <a href="#" id="one">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#" className="login">
+              <i className="fa-solid fa-right-to-bracket"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
