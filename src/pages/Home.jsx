9cdfac51dfd9259bf493022b5adfa8cf3ea2b433
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-28 12:57:51 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Home() {
   return (
     <>
-      <scetion className="home"></scetion>
+      <section className="home">
+        
+        <img src="assets/mount2.png" class="mount2" alt="mount2">
+        <img src="assets/mount1.png" class="mount1" alt="mount1">
+        <img src="assets/bush2.png" class="bush2" alt="bush2">
+        <img src="assets/bush1.png"  class="bush1"alt="bush1">
+        <h1 class="title">Website name</h1>
+        <div class="mmmm">
+        <button class="btn" type="button" onclick="openPopup()" > Start Now</button>
+         <div class="main" id="contt">
+      <div class="containerr">
+        <div class="continue">
+          <div class="cont"></div>
+      </section>
     </>
   );
 }
