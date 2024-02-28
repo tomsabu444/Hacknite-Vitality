ad99cb86712414d2fe757bbcec9eefdd5280d75f
Akash-005-fs <110680569+Akash-005-fs@users.noreply.github.com>
2024-02-28 16:32:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Studentmarks from "./component/Studentmarks";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/student-mark" element={<Studentmarks />} />
         </Routes>
       </BrowserRouter>
     </>
