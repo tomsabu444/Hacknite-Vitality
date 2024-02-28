2dc81b89264a664cae874e8c721fb1a2e5357600
Tom Sabu <tomsabu0000@gmail.com>
2024-02-28 21:05:38 +0530

    functionality OCR update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Upload from "./pages/Upload";
+import ImageToOcr from "./pages/ImageToOcr";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/upload" element={<Upload />} />
+          <Route exact path="/upload" element={<ImageToOcr />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ImageToOcr.jsx
@@ -0,0 +1,65 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useState } from "react";
+
+
+
+function ImageToOcr() {
+  const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  const FIXED_INPUT_TEXT = import.meta.env.VITE_FIXED_INPUT_TEXT; // Load from env
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchDataFromGeminiProVisionAPI() {
+    try {
+      setLoading(true);
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+      const fileInputEl = document.querySelector("input[type=file]");
+      const imageParts = await Promise.all(
+        [...fileInputEl.files].map(fileToGenerativePart)
+      );
+      const result = await model.generateContent([FIXED_INPUT_TEXT, ...imageParts]); 
+      const text = result.response.text();
+
+      setLoading(false);
+      setData(text);
+    } catch (error) {
+      setLoading(false);
+      console.error("fetchDataFromGeminiAPI error: ", error);
+    }
+  }
+
+  async function fileToGenerativePart(file) {
+    const base64EncodedDataPromise = new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result.split(",")[1]);
+    reader.readAsDataURL(file);
+    });
+    return {
+    inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+    };
+}
+
+  return (
+    <>
+      {/* ... (Other parts of your component) */}
+
+      <div className="card">
+        <input type="file" /> 
+        {/* Input text field removed */}
+
+        <button
+          disabled={loading}
+          onClick={() => fetchDataFromGeminiProVisionAPI()}
+        >
+          {loading ? "Loading..." : "Submit Button"}
+        </button>
+        <hr />
+        <div>Response: {data}</div>
+      </div>
+    </>
+  );
+}
+
+export default ImageToOcr;
