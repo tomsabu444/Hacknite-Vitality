8a3659b2d9bd0375ba28f5abbc1fc290eb438f0e
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-28 13:09:44 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
@@ -8,13 +9,15 @@ function Home() {
         <img src="src/assets/mount1.png" className="mount1" alt="mount1" />
         <img src="src/assets/bush2.png" className="bush2" alt="bush2" />
         <img src="src/assets/bush1.png" className="bush1" alt="bush1" />
-        <h1 class="title">Website name</h1>
-        <div class="mmmm">
-          <button class="btn" type="button" onclick="openPopup()">
+        <h1 className="title">Website name</h1>
+        <div className="mmmm">
+          <button className="btn" type="button" onclick="openPopup()">
             {" "}
             Start Now
           </button>
         </div>
+        <img src="assets/leaf2.png" className="leaf2 leaf" alt="leaf2" />
+        <img src="assets/leaf1.png" className="leaf1 leaf" alt="leaf1" />
       </section>
     </>
   );
