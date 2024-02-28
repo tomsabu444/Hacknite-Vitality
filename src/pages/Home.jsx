706f83a7a5e37b717384bd4afada7d86392edbcf
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-28 14:00:14 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <>
       <section className="home">
-        <img src="assets/mount2.png" className="mount2" alt="mount2" />
-        <img src="assets/mount1.png" className="mount1" alt="mount1" />
-        <img src="assets/bush2.png" className="bush2" alt="bush2" />
-        <img src="assets/bush1.png" className="bush1" alt="bush1" />
+        <img src="src/assets/mount2.png" className="mount2" alt="mount2" />
+        <img src="src/assets/mount1.png" className="mount1" alt="mount1" />
+        <img src="src/assets/bush2.png" className="bush2" alt="bush2" />
+        <img src="src/assets/bush1.png" className="bush1" alt="bush1" />
         <h1 className="title">Website name</h1>
         <div className="mmmm">
           <button className="btn" type="button" onClick={openPopup}>
@@ -69,8 +69,8 @@ function Home() {
             </div>
           </div>
         </div>
-        <img src="assets/leaf2.png" className="leaf2 leaf" alt="leaf2" />
-        <img src="assets/leaf1.png" className="leaf1 leaf" alt="leaf1" />
+        <img src="src/assets/leaf2.png" className="leaf2 leaf" alt="leaf2" />
+        <img src="src/assets/leaf1.png" className="leaf1 leaf" alt="leaf1" />
       </section>
       <section className="about">
         <div className="writtings">
