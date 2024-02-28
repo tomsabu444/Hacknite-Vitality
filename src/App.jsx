5501e57f129c291906d8f9801b9ed5e3709d2363
Tom Sabu <tomsabu0000@gmail.com>
2024-02-28 17:08:39 +0530

    frontend fixed almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Studentmarks from "./component/Studentmarks";
 
 function App() {
   return (
