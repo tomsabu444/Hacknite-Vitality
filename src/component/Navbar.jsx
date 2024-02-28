1d5265e2693dbf1cc49e7c0ce8d695d68b51413f
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-28 12:04:20 +0530

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,24 @@ function Navbar() {
   return (
     <>
       <h1>LOGO</h1>
-      <ul></ul>
+      <ul>
+        <li>
+          <a href="#" id="one">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#" class="login">
+            <i class="fa-solid fa-right-to-bracket"></i>
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
